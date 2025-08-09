a93873bbeb8fe90f5e781da6295e823f9a1057b1
Dev Raj <dr395108@gmail.com>
2025-08-09 16:13:18 +0530

    added zod

--- src/actions/create-topic.tsx
+++ src/actions/create-topic.tsx
@@ -1,6 +1,64 @@
 "use server"
-export const createTopic = async(formData:FormData) =>{
-      const title = formData.get("title")
-      const description = formData.get("description")
-      console.log(title,description)
+import { auth } from "@/auth"
+import { prisma } from "@/lib"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+import { z} from "zod"
+import type { Topic } from "@prisma/client";
+const createTopicSchema = z.object({
+      title:z.string().min(3).regex(/^[a-z-]+$/,{message:" Must be in lower case with space"}),
+      description:z.string().min(5)
+})
+type CreateTopicFormState = {
+      error?:{
+            title?:string[],
+            description?:string[],
+            formError?:string[]
+      }
+}
+export const createTopic = async(prevState:{message:string},formData:FormData) : Promise<CreateTopicFormState> =>{
+      const result = createTopicSchema.safeParse({
+            title:formData.get('title'),
+            description:formData.get('description')
+      })
+      if(!result.success){
+       return {
+            error:result.error.flatten().fieldErrors
+       }
+      }
+      const session = await auth()
+      if(!session || !session?.user){
+            return{
+                  error:{
+                        formError:["You have to login first"]
+                  }
+            }
+      }
+      console.log(result)
+      let topic : Topic;
+      try {
+            topic = await prisma.topic.create({
+                  data:{
+                        slug:result.data.title,
+                        description:result.data.description
+                  }
+            })
+      } catch (error) {
+            if(error instanceof Error){
+                  return {
+                        error:{
+                              formError:[error.message]
+                        }
+                  }
+            }
+            else{
+                  return{
+                        error:{
+                              formError:["Something went wrong"]
+                        }
+                  }
+            }
+      }
+      revalidatePath('/')
+      redirect(`/topics/${topic.slug}`)
 }
--- src/components/topics/AddTopics.tsx
+++ src/components/topics/AddTopics.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+"use client"
+import React, { useActionState } from 'react'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogFooter,
   DialogTrigger
 } from "@/components/ui/dialog"
 import { Button } from '../ui/button'
@@ -13,7 +13,9 @@ import { Input } from '../ui/input'
 import { Textarea } from '../ui/textarea'
 import { Label } from '../ui/label'
 import { createTopic } from '@/actions/create-topic'
+
 const AddTopics = () => {
+      const [formState,action] = useActionState(createTopic,{error:{}})
   return (
     <div>
        <Dialog>
@@ -25,19 +27,34 @@ const AddTopics = () => {
                         <DialogTitle>Discuss New Topics</DialogTitle>
                         <DialogDescription>Add a new topic so everyone can discuss</DialogDescription>
                   </DialogHeader>
-                   <form action={createTopic}>
+                   <form action={action}>
                   <div>
                         <div className='my-2'>
                               <Label htmlFor='title' className='my-2'>Title</Label>
                               <Input type='text' id='title' name='title' placeholder='type title here...'></Input>
+                              {
+                              formState.error?.title &&
+                              <p className='text-red-500'>{formState.error.title}</p>
+                        }
                         </div>
                          <div className='my-2'>
                               <Label htmlFor='description' className='my-2'>Description</Label>
                               <Textarea id='description' name='description' placeholder='type description here...' className='h-40'/>
+                        {
+                              formState.error?.description &&
+                              <p className='text-red-500'>{formState.error.description}</p>
+                        }
+                        {
+                              formState.error?.formError &&
+                              <div className='my-2 '>
+                              <p className='text-red-500'>Title already exits</p>
+                              </div>
+                        }
                         </div>
                         <div className='text-center'>
                              <Button type='submit'>Add</Button>
                         </div>
+                        
                   </div>
                    </form>
             </DialogContent>
